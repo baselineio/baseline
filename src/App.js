13df6e0e169271576140232e2466222e85e07e8c
Eric Walter <eric.walter@eimprovement.com>
2019-07-09 22:26:18 -0500

    add baseline app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey Kevin! We have a starter application hosted on a server!
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
